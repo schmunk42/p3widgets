dc19903a4071c1102dc70579fa647c5ed228c02d
Tobias Munk <schmunk@usrbin.de>
2011-10-18 01:26:39 +0200

    cleanup

--- config/main.php
+++ config/main.php
@@ -7,10 +7,6 @@ return array(
 			'params' => array(
 				'widgets' => array(
 					'CWidget' => 'Basic Widget',
-					'zii.widgets.CMenu' => 'Menu Widget',
-					'ext.yiiext.widgets.fancybox.EFancyboxWidget'	=> 'Fancy Box',
-					'ext.yiiext.widgets.cycle.ECycleWidget'			=> 'Cycle',
-					#'ext.yiiext.widgets.carousel.ECarouselWidget'	=> 'Carousel',
 				)
 			)
 		)
