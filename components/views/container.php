8e7ae219f5fae6bfabd7d00b0cbb662940289a53
Tobias Munk <schmunk@usrbin.de>
2011-11-01 19:42:21 +0100

    added parent CSS class placeholder

--- components/views/container.php
+++ components/views/container.php
@@ -1,10 +1,10 @@
 	<div id="<?php echo P3WidgetContainer::CONTAINER_CSS_PREFIX . $this->id ?>" class="widget-container admin">
 		<div class="header">
-			<h1>Container <?php echo $this->id ?></h1>
+			<h1>Container <?php echo $this->id ?> [<span class="cssClasses"></span>]</h1>			
 		<?php
 		$this->widget('zii.widgets.jui.CJuiButton', array(
 			'buttonType' => 'link',
-			'url' => array('/p3widgets/widget/create', 'Widget' => $widgetAttributes),
+			'url' => array('/p3widgets/widget/create', 'Widget' => $widgetAttributes, 'returnUrl' => Yii::app()->request->getUrl()),
 			'name' => 'btnClick' . uniqid(),
 			'options' => array('icons' => 'js:{primary:"ui-icon-plus"}'),
 			#'onclick' => 'js:function(){alert("clicked"); this.blur(); return false;}',
