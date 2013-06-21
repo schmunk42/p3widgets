9399988cda5c3162b8c98b783372edf88655ca14
Tobias Munk <schmunk@usrbin.de>
2013-06-21 10:53:25 +0200

    added language fallback handling

--- components/P3ReferenceWidget.php
+++ components/P3ReferenceWidget.php
@@ -6,18 +6,20 @@
 class P3ReferenceWidget extends CWidget
 {
     public $widgetId;
+    public $translationLanguage = null;
+    public $translationFallback = true;
     public $overrideAttributes = array();
 
     function run()
     {
         $model = P3Widget::model()->findByPk($this->widgetId);
         if ($model !== null) {
-            $properties = CMap::mergeArray(CJSON::decode($model->t('properties')), $this->overrideAttributes);
+            $properties = CMap::mergeArray(CJSON::decode($model->t('properties', $this->translationLanguage, $this->translationFallback)), $this->overrideAttributes);
             if (Yii::app()->user->checkAccess('P3widgets.Widget.*')) {
                 echo "<div class='alert alert-info container-message'>Copy of widget #{$model->id}</div>";
             }
             $this->beginWidget($model->alias, $properties);
-            echo $model->t('content');
+            echo $model->t('content', $this->translationLanguage, $this->translationFallback);
             $this->endWidget();
         } else {
             trigger_error("Reference Widget #" . $this->widgetId . " not found!", E_USER_WARNING);
