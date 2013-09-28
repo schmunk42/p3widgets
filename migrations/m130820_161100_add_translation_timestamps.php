e13dedb553a85eeda7d38909a32dc1b9500c6e1a
Tobias Munk <schmunk@usrbin.de>
2013-09-28 21:23:01 +0200

    fixed missing default values in migration

--- migrations/m130820_161100_add_translation_timestamps.php
+++ migrations/m130820_161100_add_translation_timestamps.php
@@ -4,8 +4,8 @@ class m130820_161100_add_translation_timestamps extends EDbMigration
 {
     public function up()
     {
-        $this->addColumn('p3_widget_translation','createdAt','DATETIME NOT NULL');
-        $this->addColumn('p3_widget_translation','modifiedAt','DATETIME NOT NULL');
+        $this->addColumn('p3_widget_translation','createdAt','DATETIME NOT NULL DEFAULT "0000-00-00"');
+        $this->addColumn('p3_widget_translation','modifiedAt','DATETIME NOT NULL DEFAULT "0000-00-00"');
     }
 
     public function down()
