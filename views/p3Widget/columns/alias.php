e4c71ac618efd1fc63c6978d7915f3820252810d
Christopher Stebe <chris@stebe.eu>
2014-02-17 14:18:56 +0100

    update p3widget alias column -> parse widgets before render dropdown (from new checkAccess config feature)

--- views/p3Widget/columns/alias.php
+++ views/p3Widget/columns/alias.php
@@ -1,6 +1,22 @@
-<?php echo $form->dropDownList(
-    $model,
-    'alias',
-    $this->module->params['widgets'],
-    array('onchange' => 'updateProperties()')
-);
+<?php
+    // parse widgets
+    $p3widgets = array();
+    foreach ($this->module->params['widgets'] AS $alias => $config) {
+        if (is_array($config)) {
+            if (isset($config['checkAccess']) && !Yii::app()->user->checkAccess($config['checkAccess'])) {
+                continue;
+            }
+            $p3widgets[$alias] = $config['name'];
+        } else {
+            $p3widgets[$alias] = $config;
+        }
+    }
+    // render dropdown
+    echo $form->dropDownList(
+        $model,
+        'alias',
+        $p3widgets,
+        array(
+            'onchange' => 'updateProperties()'
+        )
+    );
